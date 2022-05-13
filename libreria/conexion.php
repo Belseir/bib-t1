9aa238697b20f7f145046a0ad8e57acd8ccb2125
Fabricio Futryk <ffutryk52@gmail.com>
2022-05-13 18:21:47 -0300

    style: Removed useless comments

--- libreria/conexion.php
+++ libreria/conexion.php
@@ -1,17 +1,15 @@
 <?php
-class Conexion extends mysqli{
+class Conexion extends mysqli
+{
 
- public $enlace;
- 
- function __construct(){
-   //$this->enlace=mysql_connect(DB_HOST,DB_USER,DB_PASS);
-   //mysql_select_db(DB_NAME,$this->enlace);
-   $this->enlace=mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
-    
-  
- }
- function __destruct(){
-   //mysql_close($this->enlace);
-   mysqli_close($this->enlace);
- }
+  public $enlace;
+
+  function __construct()
+  {
+    $this->enlace = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+  }
+  function __destruct()
+  {
+    mysqli_close($this->enlace);
+  }
 }
